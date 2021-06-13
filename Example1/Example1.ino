739ec7370136e4c7f2d10438c7f74517814decad
aminedachir <dachiramine110@gmail.com>
2021-06-13 23:12:35 +0100

    analogRead()

--- Example1/Example1.ino
+++ Example1/Example1.ino
@@ -1,9 +1,10 @@
 int led = 10;
 int lx = 100;
+int c = analogRead(A0);
 void setup(){
   pinMode(led,OUTPUT);
 }
 void loop(){
-  if (analogRead(A0)>lx){digitalWrite(led,LOW);}
+  if (c>lx){digitalWrite(led,LOW);}
   else {digitalWrite(led,HIGH);}
 }
