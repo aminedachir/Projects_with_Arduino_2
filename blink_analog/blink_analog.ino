c9a4f15142c3f84f666f551d9d68e5edfd871f58
aminedachir <dachiramine110@gmail.com>
2021-06-14 15:31:19 +0100

    add condition

--- blink_analog/blink_analog.ino
+++ blink_analog/blink_analog.ino
@@ -1,12 +1,12 @@
 int led = 10;
-int D = 500;
+int D = 70;
 void setup() {
   pinMode(10,OUTPUT);
 }
 
 void loop() {
-  static int x = 0;
+  static  int x = 17;
   analogWrite(led,x);
   delay(D);
-  x = x + 10;
+  if (x < 255) {x=x+17;}
 }
