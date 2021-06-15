090a417778af78a2877266a3fb92e50f2eb816ed
aminedachir <dachiramine110@gmail.com>
2021-06-15 12:02:45 +0100

    change color agter 1sec

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,14 +1,18 @@
-int RED = 200;
-int GREEN = 30;
-int BLUE = 80;
+int R = 9;
+int G = 10;
+int B = 11;
 void setup() {
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
-  analogWrite(11,RED);
-  analogWrite(9,GREEN);
-  analogWrite(10,BLUE);
 }
 
 void loop() {
+  int RV = random(0,255);
+  int GV = random(0,255);
+  int BN = random(0,255);
+  analogWrite(R,RV);
+  analogWrite(G,GV);
+  analogWrite(B,BV);
+  delay(1000);
 }
