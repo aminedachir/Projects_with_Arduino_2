8cdf73df204c25416c3a4b219fee6396a3e8226d
aminedachir <dachiramine110@gmail.com>
2021-06-15 15:32:31 +0100

    print numbers in serial monitor

--- serialmonitor/serialmonitor.ino
+++ serialmonitor/serialmonitor.ino
@@ -1,9 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  for (int i=0;i<=10;i++){
+    Serial.print(i);
+    Serial.print("\n");
+    delay(300); 
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
