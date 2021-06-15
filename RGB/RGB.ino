823705ffc15e3a56c66031e53a986e87408242de
aminedachir <dachiramine110@gmail.com>
2021-06-15 12:15:05 +0100

    change color with random function

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -6,15 +6,13 @@ void setup() {
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
 }
-
 void loop() {
   static int x = 0;
   int RV = random(0,255);
   int GV = random(0,255);
   int BN = random(0,255);
-  if (x<255) {x=x+50,analogWrite(R,x=x+50),
-  analogWrite(G,x=x+30),
-  analogWrite(B,x=x+10);  
-  delay(1000);}
-  else {x = 0,analogWrite(R,x),analogWrite(G,x),analogWrite(B,x),delay(1000);}
+  analogWrite(R,RV);
+  analogWrite(G,GV);
+  analogWrite(B,BV);  
+  delay(1000);
 }
