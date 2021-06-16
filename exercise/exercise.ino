8542fd31283e0494c185fb6e1183522bbc98f8aa
aminedachir <dachiramine110@gmail.com>
2021-06-16 11:48:33 +0100

    led

--- exercise/exercise.ino
+++ exercise/exercise.ino
@@ -4,15 +4,16 @@ int y;
 void setup() {
   Serial.begin(9600);
   pinMode(led,OUTPUT);
-}
-
-void loop() {
   Serial.println("How many blinks :");
   while (Serial.available() == 0) {;}
   x = Serial.parseInt();
+  delay(500);
   Serial.println("How many Delay time in ms :");
   while (Serial.available() == 0) {;}
   y = Serial.parseInt();
+}
+
+void loop() {
   for (int i=0;i<x;i++){
     digitalWrite(led,1);
     delay(y);
