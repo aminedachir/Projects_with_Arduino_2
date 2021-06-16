f6e35b031c40c342bf3faf85fe07dc841f12bb17
aminedachir <dachiramine110@gmail.com>
2021-06-16 12:25:42 +0100

    solutions

--- exercise/exercise.ino
+++ exercise/exercise.ino
@@ -1,22 +1,19 @@
 int led = 10;
-int x;
-int y;
 void setup() {
-  Serial.begin(9600);
   pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
   Serial.println("How many blinks :");
   while (Serial.available() == 0) {;}
-  x = Serial.parseInt();
-}
-
-void loop() {
-  for (int i=0;i<x;i++){
-    digitalWrite(led,1);
-    delay(500);
-    digitalWrite(led,0);
-    delay(500);
+  int x = Serial.parseInt();
+  Serial.println("How many Delay Time :");
+  while (Serial.available() == 0) {;}
+  int y = Serial.parseInt();
+  for (int i=x;i>0;i--){
+    digitalWrite(led,HIGH);
+    delay(y);
+    digitalWrite(led,LOW);
+    delay(y);
     }
-   delay(2500);
-  
-  
 }
