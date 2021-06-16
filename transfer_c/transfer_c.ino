3ec429fa6e73e51b86a8fc1e967a62c07e5dd21a
aminedachir <dachiramine110@gmail.com>
2021-06-16 12:42:19 +0100

    finish

--- transfer_c/transfer_c.ino
+++ transfer_c/transfer_c.ino
@@ -1,7 +1,10 @@
-float c = 23.65;
+float C;
 void setup() {
   Serial.begin(9600);
-  float F = c*9/5+32;
+  Serial.println("Enter Value of C° :");
+  while (Serial.available() == 0) {;}
+  float C = Serial.parseFloat();
+  float F = C*9/5+32;
   Serial.print("F = ");
   Serial.println(F); 
 }
