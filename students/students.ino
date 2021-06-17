8b8b2e690cd84822e151c9384ddfb563e5a150f4
aminedachir <dachiramine110@gmail.com>
2021-06-17 13:17:15 +0100

    avg

--- students/students.ino
+++ students/students.ino
@@ -1,8 +1,10 @@
 int sum;
+int moyenn;
 void setup() {
   Serial.begin(9600);
   int students [ ] = {12,8,5,15,19,4,20,4.5,8.5,13};
   for (int i=0;i<10;i++) {int sum = sum + students[i];}
+  int moyenn = sum/10
 }
 
 void loop() {
