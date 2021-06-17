e0bf9879bfd899e002a2e28c5c6a350dd8c48b0f
aminedachir <dachiramine110@gmail.com>
2021-06-17 13:10:11 +0100

    finish

--- students/students.ino
+++ students/students.ino
@@ -1,10 +1,8 @@
 void setup() {
   Serial.begin(9600);
-  int students [ ] = {12,8,5,15,19,2};
-  for (int i=0;i<6;i++){
-    int moyenn = students[i] + students[i+1];
-    Serial.println(moyenn);  
-    ;}
+  int a [ ] = {12,8,5};
+  float moyenn = (a[0]+a[1]+a[2])/3;
+  Serial.println(moyenn);
 }
 
 void loop() {
