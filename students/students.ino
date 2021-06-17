4270ceb94bbf08a241fb369167a277bc75590531
aminedachir <dachiramine110@gmail.com>
2021-06-17 13:01:59 +0100

    use serial monitor

--- students/students.ino
+++ students/students.ino
@@ -1,8 +1,9 @@
 void setup() {
+  Serial.begin(9600);
   int students [ ] = {12,8,5,15,19,2};
   for (int i=0;i<6;i++){
-    int moyenn = a[i] + a[i+1];
-    
+    int moyenn = students[i] + students[i+1];
+    Serial.println(moyenn);  
     ;}
 }
 
