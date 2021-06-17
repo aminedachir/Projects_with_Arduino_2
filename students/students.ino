1862aaf1c9164931b0ea80d6d3d737a01b3b1a1b
aminedachir <dachiramine110@gmail.com>
2021-06-17 13:22:14 +0100

    use serial monitor

--- students/students.ino
+++ students/students.ino
@@ -1,10 +1,10 @@
+float students[ ]={12.0,8.0,5.0,15.0,19.0,4.0,20.0,4.5,8.5,13.6};
 float sum = 0;
 float moyenn = 0;
 void setup() {
   Serial.begin(9600);
-  float students [ ] = {12.0,8.0,5.0,15.0,19.0,4.0,20.0,4.5,8.5,13.6};
-  for (int i=0;i<10;i++) {float sum = sum + students[i];}
-  float moyenn = sum/10;
+  for (int i=0;i<10;i++) {sum = sum + students[i];}
+  moyenn = sum/10.0;
   Serial.println(moyenn);
 }
 
