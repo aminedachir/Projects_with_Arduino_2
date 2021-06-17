b2b5c98c4a15c7ed332015bdddc604b071eb5cfb
aminedachir <dachiramine110@gmail.com>
2021-06-17 13:19:24 +0100

    float

--- students/students.ino
+++ students/students.ino
@@ -1,10 +1,11 @@
-int sum;
-int moyenn;
+float sum = 0;
+float moyenn = 0;
 void setup() {
   Serial.begin(9600);
-  int students [ ] = {12,8,5,15,19,4,20,4.5,8.5,13};
-  for (int i=0;i<10;i++) {int sum = sum + students[i];}
-  int moyenn = sum/10
+  float students [ ] = {12.0,8.0,5.0,15.0,19.0,4.0,20.0,4.5,8.5,13.6};
+  for (int i=0;i<10;i++) {float sum = sum + students[i];}
+  float moyenn = sum/10;
+  Serial.println(moyenn);
 }
 
 void loop() {
