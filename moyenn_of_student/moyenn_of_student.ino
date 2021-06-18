7480bbdd819802d092c5b19aa0aaa99190dad875
aminedachir <dachiramine110@gmail.com>
2021-06-18 12:30:45 +0100

    use map

--- moyenn_of_student/moyenn_of_student.ino
+++ moyenn_of_student/moyenn_of_student.ino
@@ -1,10 +1,10 @@
-float moyenn = 3.78,y;
+float moyenn_4 = 3.78;
 void setup() {
   Serial.begin(9600);
-  float parsent = map(moyenn*100,0,4,0,100)/100.0;
+  float parsent = map(moyenn_4*100,0,4,0,100)/100.0;
   Serial.println(parsent);
-  float moyenn2 = map(parsent*100,0,100,0,5)/100.0;
-  Serial.println(moyenn2);
+  float moyenn_5 = map(parsent*100,0,100,0,5)/100.0;
+  Serial.println(moyenn_5);
 }
 void loop() {
 }
