c9717cd09006061183e74d3a25eb8e5409fe0a59
aminedachir <dachiramine110@gmail.com>
2021-06-19 14:20:45 +0100

    void setup

--- random__array/random__array.ino
+++ random__array/random__array.ino
@@ -1,13 +1,13 @@
-int array_ [] = {};
+int array_[3];
 int x;
 void setup() {
   Serial.begin(9600);
+  for(int i=0;i<3;i++) {
+    array_[i] = random(70,100);
+    Serial.println(array_[i]);
+    delay(2000);
+  ;}
 }
 
 void loop() {
-  for(int i=0;i<=18;i++) {
-    x = random(70,100);
-    array_[i] == x;
-  ;}
-  Serial.println(array_[18]);
 }
