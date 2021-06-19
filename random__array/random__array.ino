1653a928be0b184135fc2ca655635b20acfd4fe3
aminedachir <dachiramine110@gmail.com>
2021-06-19 14:07:36 +0100

    use Serial monitor

--- random__array/random__array.ino
+++ random__array/random__array.ino
@@ -1,13 +1,14 @@
 int array_ [18] = {};
 int x;
 void setup() {
-
+  Serial.begin(9600);
 }
 
 void loop() {
   for(int i=0;i<=18;i++) {
     x = random(70,100);
-    array_[]
+    array_[i] == x;
+    
     
     ;}
 }
