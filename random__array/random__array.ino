a27e1992f37d35b8ef93d70b70196698744f0c08
aminedachir <dachiramine110@gmail.com>
2021-06-19 14:38:18 +0100

    finish

--- random__array/random__array.ino
+++ random__array/random__array.ino
@@ -1,16 +1,15 @@
-int array_[3],max_,x;
+int array_[18],max_=0,x;
 void setup() {
   Serial.begin(9600);
-  for(int i=0;i<3;i++) {
+  for(int i=0;i<18;i++) {
     array_[i] = random(70,100);
     Serial.println(array_[i]);
-    delay(2000);
     }
    for (int i=0;i<3;i++){
     int max_ = max(max_,array_[i]);
-    ;}
     Serial.print("max :");
     Serial.println(max_);
+    ;}
 }
 void loop() {
 }
