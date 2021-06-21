b6f5ee8dc7e533449fbe5b5e0218c117bf983439
aminedachir <dachiramine110@gmail.com>
2021-06-21 14:39:23 +0100

    Varibaless

--- bcd_to_gray/bcd_to_gray.ino
+++ bcd_to_gray/bcd_to_gray.ino
@@ -1,18 +1,17 @@
-int number;
-int number_=00;
+int number,number_=00,x,y,z,l;
 void setup() {
   Serial.begin(9600);
   Serial.println("enter a number in bcd :");
   while (Serial.available() == 0) {;}
-  int z = Serial.parseInt();
+  z=Serial.parseInt();
   Serial.println(z);
-  int x=bitRead(number,0);
-  int y=bitWrite(number_,0,x);
+  x=bitRead(number,0);
+  y=bitWrite(number_,0,x);
   for (int i=0;i<3;i++){
-    int x = bitRead(number,i);
-    int l = bitRead(number,i+1);
-    if (x == l) {int y=bitWrite(number_,i+1,0);}
-    else {int y=bitWrite(number_,i+1,1);}
+    x=bitRead(number,i);
+    l=bitRead(number,i+1);
+    if (x==l) {y=bitWrite(number_,i+1,0);}
+    else {y=bitWrite(number_,i+1,1);}
   ;}
   Serial.println(number_);
   }
