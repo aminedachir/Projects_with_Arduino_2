17ef8beb694ac2d9fb2c4538758d15c35f511389
aminedachir <dachiramine110@gmail.com>
2021-06-21 14:38:00 +0100

    bcd to gray

--- bcd_to_gray/bcd_to_gray.ino
+++ bcd_to_gray/bcd_to_gray.ino
@@ -1,13 +1,13 @@
 int number;
-int number_ = 00;
+int number_=00;
 void setup() {
   Serial.begin(9600);
   Serial.println("enter a number in bcd :");
   while (Serial.available() == 0) {;}
   int z = Serial.parseInt();
   Serial.println(z);
-  int x = bitRead(number,0);
-  int y = bitWrite(number_,0,x);
+  int x=bitRead(number,0);
+  int y=bitWrite(number_,0,x);
   for (int i=0;i<3;i++){
     int x = bitRead(number,i);
     int l = bitRead(number,i+1);
