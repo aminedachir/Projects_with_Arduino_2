255be60eec6771fae607e49ddff8dbdcc73d4d67
aminedachir <dachiramine110@gmail.com>
2021-06-30 15:08:23 +0100

    max value

--- calibrations_ldr/calibrations_ldr.ino
+++ calibrations_ldr/calibrations_ldr.ino
@@ -2,7 +2,10 @@ int min_ldr = 1023;
 int max_ldr = 0;
 int x=0,z=0;
 void setup() {
-
+  pinMode(12,OUTPUT);
+  while(millis() < 5000){
+    max_ldr = max(max_ldr, analogRead(A0);
+  }
 }
 
 void loop() {
