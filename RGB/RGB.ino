bb0f52de7b6a872e48f8cae86afa5c7bd702fabb
aminedachir <dachiramine110@gmail.com>
2021-07-04 16:39:51 +0100

    RGB LED

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,24 +1,5 @@
-int R = 9;
-int G = 10;
-int B = 11;
 void setup() {
-  pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-  int RV = random(0,255);
-  int GV = random(0,255);
-  int BV = random(0,255);
-  analogWrite(R,200);
-  analogWrite(G,GV);
-  analogWrite(B,BV);
-}
+  }
 void loop() {
-  /* static int x = 0;
-  int RV = random(0,255);
-  int GV = random(0,255);
-  int BV = random(0,255);
-  analogWrite(R,RV);
-  analogWrite(G,GV);
-  analogWrite(B,BV);  
-  */
+  analogWrite(A0,255);
 }
