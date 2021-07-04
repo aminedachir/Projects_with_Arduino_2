d86c888a55540d7d84da735d2f95f9597e08bed0
aminedachir <dachiramine110@gmail.com>
2021-07-04 16:49:14 +0100

    RGB

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,5 +1,18 @@
+int R = 9;
+int G = 10;
+int B = 11;
 void setup() {
-  }
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+}
 void loop() {
-  analogWrite(A0,255);
+  static int x = 0;
+  int RV = random(0,255);
+  int GV = random(0,255);
+  int BV = random(0,255);
+  analogWrite(A0,RV);
+  analogWrite(G,GV);
+  analogWrite(B,BV);  
+  delay(550);
 }
