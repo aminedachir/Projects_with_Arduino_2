1fab00f7371f8274f9ff3b7e68cc9273b357edd6
aminedachir <dachiramine110@gmail.com>
2021-08-11 01:42:05 +0100

    led

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -4,6 +4,8 @@
 #endif
 #define LED_PIN    5
 #define LED_COUNT 60
+int a = 0;
+int b = 0;
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -14,14 +16,21 @@ void setup() {
   strip.setBrightness(50); 
 }
 void loop() {
-  colorWipe(strip.Color(255,   0,   0), 250); // Red
-  colorWipe(strip.Color(  0, 255,   0), 250); // Green
-  colorWipe(strip.Color(  0,   0, 255), 250); // Blue
+  for (int i=0;i<5;i++) {
+    colorWipe(strip.Color(a,   a,   a), 250); 
+    colorWipe(strip.Color(  0, 0,   0), 50); 
+    a = a + 50;
+  }
+  delay(200);
+  for (int c=0;c<5;c++) {
+    colorWipe(strip.Color(b, b-20, b-10),50);
+    b = b + 50;
+  }
 }
 void colorWipe(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, color);         
     strip.show();                          
-    delay(50);                           
+    delay(10);                           
   }
 }
