6fedffd0affb5284950a759d63bc8ffa4f5ef84f
aminedachir <dachiramine110@gmail.com>
2021-08-12 00:34:13 +0100

    speed

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,41 +1,22 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> 
-#endif
 #define LED_PIN    5
-#define LED_COUNT 60
-int a = 50;
-int b = 50;
+#define LED_COUNT 16
+int a = 250;
+int b = 0;
 int c = 0;
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  strip.begin();           
-  strip.show();            
-  strip.setBrightness(255); 
 }
 void loop() {
-  for (int i=0;i<10;i++){
+  for (int i=0;i<50;i++){
+    delay(500);
+    Adafruit_NeoPixel strip(i , LED_PIN);
+    strip.begin();           
+    strip.show();            
+    strip.setBrightness(255);
     colorWipe(strip.Color(a,  b,   0), 50);  
-    a = a + 5;
-    b = b + 10;
+    a = a - 25;
+    if (a==25) {b = b + 25;}
     }
-  delay(100);
-  a = 0;
-  b = 0;
-  for (int i=0;i<10;i++){
-    colorWipe(strip.Color(0,  b,   0), 50);
-    b = b + 5;
-    }
-  delay(500);
-  b = 0;
+  delay(2000);
 }
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, color);         
-    strip.show();                          
-    delay(10);                           
-  }
-}
+void colorWipe(uint32_t color, int wait) {}
