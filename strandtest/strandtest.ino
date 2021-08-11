3a7c7c30c66656509bdc93e57301272f33b2858f
aminedachir <dachiramine110@gmail.com>
2021-08-12 00:51:00 +0100

    solve

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,18 +1,15 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN    5
-int a = 250;
-int b = 250;
-int c = 50;
 void setup() {
 }
 void loop() {
   for (int i=0;i<16;i++){
     Adafruit_NeoPixel strip(i , LED_PIN);
-    delay(1000);
+    delay(500);
     strip.begin();           
     strip.show();            
     strip.setBrightness(255);
-    colorWipe(strip.Color(a,  b,   0), 50);  
+    //colorWipe(strip.Color(a,  b,   0), 50);  
     }
 }
 void colorWipe(uint32_t color, int wait) {}
