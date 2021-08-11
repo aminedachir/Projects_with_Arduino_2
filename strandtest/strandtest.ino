9098b1394172e7f4a892b3970c01d26507f23147
aminedachir <dachiramine110@gmail.com>
2021-08-11 16:01:19 +0100

    yellow

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -4,8 +4,8 @@
 #endif
 #define LED_PIN    5
 #define LED_COUNT 60
-int a = 0;
-int b = 0;
+int a = 50;
+int b = 50;
 int c = 0;
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
@@ -17,15 +17,17 @@ void setup() {
   strip.setBrightness(255); 
 }
 void loop() {
-  for (int i=0;i<6;i++){
-    colorWipe(strip.Color(a,  0,   0), 50);  
-    a = a + 25;
+  for (int i=0;i<10;i++){
+    colorWipe(strip.Color(a,  b,   0), 50);  
+    a = a + 5;
+    b = b + 10;
     }
-  delay(500);
+  delay(100);
   a = 0;
-  for (int i=0;i<6;i++){
+  b = 0;
+  for (int i=0;i<10;i++){
     colorWipe(strip.Color(0,  b,   0), 50);
-    b = b + 25;
+    b = b + 5;
     }
   delay(500);
   b = 0;
