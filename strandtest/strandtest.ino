8f4d48999a981b4e790c5fa0d1d1e7f3e36feaae
aminedachir <dachiramine110@gmail.com>
2021-08-12 00:44:38 +0100

    color white

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,19 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN    5
 int a = 250;
-int b = 0;
-int c = 0;
+int b = 250;
+int c = 50;
 void setup() {
 }
 void loop() {
-  for (int i=0;i<5;i++){
+  for (int i=0;i<16;i++){
     Adafruit_NeoPixel strip(i , LED_PIN);
-    colorWipe(strip.Color(a,  b,   0), 50); 
+    //colorWipe(strip.Color(a,  b,   0), 50); 
     delay(1000);
     strip.begin();           
     strip.show();            
     strip.setBrightness(255);
-    colorWipe(strip.Color(a,  b,   0), 50);  
+    //colorWipe(strip.Color(a,  b,   0), 50);  
     }
 }
 void colorWipe(uint32_t color, int wait) {}
