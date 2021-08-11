eaae196446be98475d885da0ba0fe507f37dc62e
aminedachir <dachiramine110@gmail.com>
2021-08-12 00:51:59 +0100

    colorwipe

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -3,13 +3,11 @@
 void setup() {
 }
 void loop() {
-  for (int i=0;i<16;i++){
+  for (int i=0;i<160;i++){
     Adafruit_NeoPixel strip(i , LED_PIN);
     delay(500);
     strip.begin();           
     strip.show();            
-    strip.setBrightness(255);
-    //colorWipe(strip.Color(a,  b,   0), 50);  
+    strip.setBrightness(255);  
     }
 }
-void colorWipe(uint32_t color, int wait) {}
