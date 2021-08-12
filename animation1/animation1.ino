38288f287fac08cc4f7d14df51ac9458d39f7d80
aminedachir <dachiramine110@gmail.com>
2021-08-12 21:24:11 +0100

    animation2

--- animation1/animation1.ino
+++ animation1/animation1.ino
@@ -6,13 +6,11 @@
 #define LED_COUNT 15
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
   strip.begin(); 
   strip.show(); 
-  strip.setBrightness(50);
+  strip.setBrightness(255);
   animation1(strip.Color(  255,   255, 255),450);
+  //rainbow(10);
   animation1(strip.Color(  0,   0, 0),0);
 }
 void loop() {
@@ -28,3 +26,13 @@ void animation1(uint32_t color,int wait) {
     delay(wait);
   }
 } 
+void rainbow(int wait) {
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) {
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show();
+    delay(wait); 
+  }
+}
