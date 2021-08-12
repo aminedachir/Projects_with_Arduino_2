f396c67a494070ea6fd4403e500ceaf05abbcad0
aminedachir <dachiramine110@gmail.com>
2021-08-12 01:12:37 +0100

    color speed

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -1,13 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN   5
-void setup() {
-}
+void setup() {}
 void loop() {
   for (int i=0;i<160;i++){
     Adafruit_NeoPixel strip(i , LED_PIN);
     delay(500);
-    strip.begin();           
-    strip.show();            
+    strip.begin();
+    strip.show();
     strip.setBrightness(255);  
     }
 }
