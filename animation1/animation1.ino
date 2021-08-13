441353a776927ac73edcad8b3b155cebb1147f55
aminedachir <dachiramine110@gmail.com>
2021-08-13 23:09:10 +0100

    three leds

--- animation1/animation1.ino
+++ animation1/animation1.ino
@@ -3,24 +3,27 @@
  #include <avr/power.h>
 #endif
 #define LED_PIN    5
-int a=3;
-#define LED_COUNT 15
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define LED_COUNT 50
+int a = random(0,255);
+int b = random(0,255);
+int c = random(0,255);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN,NEO_GRB + NEO_KHZ800);
 void setup() {
   strip.begin(); 
   strip.show(); 
   strip.setBrightness(255);
-  //animation_1(strip.Color(  255,   255, 255),400);
-  //animation_2(7.5);
-  //animation_1(strip.Color(  0,   0, 0),400);
-  animation_3(strip.Color(  255,   255, 255),400);
-  animation_3(strip.Color(  0,   0, 0),400);
+  //animation_1(strip.Color(  255,   155, 255),200);
+  //animation_2(2);
+  //animation_1(strip.Color(  0,   0, 0),200);
+  three_leds(strip.Color(  a,   b, c),100);
+  //delay(500);
+  three_leds(strip.Color(  0,   0, 0),400);
   //animation_3(500);
 }
 void loop() {   
 }
 void animation_1(uint32_t color,int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { 
+  for(int i=0; i<LED_COUNT; i++) { 
     strip.setPixelColor(i, color);
     strip.show();                          
     delay(wait);
@@ -36,15 +39,14 @@ void animation_2(int wait) {
     delay(wait); 
   }
 }
-void animation_3(uint32_t color,int wait) {
-  for(int i=0; i<3; i++) { 
-    strip.setPixelColor(i, color);
+void three_leds(uint32_t color,int wait) {
+  for(int i=0; i<LED_COUNT; i++) { 
+    strip.setPixelColor(i, strip.Color(a,b,c));
+    strip.setPixelColor(i+1, strip.Color(a,b,0));
+    strip.setPixelColor(i+2, strip.Color(a,0,c));
+    strip.setPixelColor(i-1,strip.Color(0,0,0));
     strip.show();                          
     delay(wait);
-  }
-  for(int i=3; i<6; i++) { 
-    strip.setPixelColor(i, color);
-    strip.show();                          
-    delay(wait);
+    
   }
 }
