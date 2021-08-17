2d0c6fc956e76434699c699771960e8eec4d7aa6
aminedachir <dachiramine110@gmail.com>
2021-08-17 01:43:36 +0100

    rainbow

--- animation1/animation1.ino
+++ animation1/animation1.ino
@@ -11,7 +11,7 @@ void setup() {
   strip.show(); 
   strip.setBrightness(255);
   //animation_1(strip.Color(  255,   155, 255),200);
-  //animation_2(2);
+  animation_2(2);
   //animation_1(strip.Color(  0,   0, 0),200);
   //three_leds(strip.Color(  155,   155, 0),100);
   //delay(500);
@@ -23,10 +23,10 @@ void setup() {
   //five_leds(strip.Color(  255,   30, 0),100);
 }
 void loop() {   
-  int a = random(0,255);
-  int b = random(0,255);
-  int c = random(0,255);
-  random_colors(strip.Color(  a,   b, c),100);
+  //int a = random(0,255);
+  //int b = random(0,255);
+  //int c = random(0,255);
+  //random_colors(strip.Color(  a,   b, c),100);
 }
 void animation_1(uint32_t color,int wait) {
   for(int i=0; i<LED_COUNT; i++) { 
@@ -39,13 +39,16 @@ void animation_1(uint32_t color,int wait) {
   }
 } 
 void animation_2(int wait) {
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) {
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+  for(int i=0; i<strip.numPixels(); i++) {
+    for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+      //int pixelHue = random(0,255);
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(firstPixelHue)));
+      //delay(50);
+      strip.show();
+      delay(wait);
     }
-    strip.show();
-    delay(wait); 
+    //strip.show();
+    //delay(wait); 
   }
 }
 void three_leds(uint32_t color,int wait) {
